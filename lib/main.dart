3caa21fd7933f339f3cb06d7a215694ba8208967
Hrihoriy <hryhoriy.kysil@chisw.com>
2023-01-08 21:15:01 +0200

    refactor: remove localisation code from main() to wrapper widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,7 @@ void main() async {
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
 
-  //todo move locale wrapper to separate class
-  runApp(EasyLocalization(
-    path: 'assets/translations',
-    supportedLocales: const [
-      Locale('en'),
-      Locale('ru'),
-    ],
-    assetLoader: const CodegenLoader(),
-    fallbackLocale: const Locale('en'),
-    child: MyApp(),
-  ));
+  runApp(const EasyLocalisationWrapper());
 }
 
 class MyApp extends StatelessWidget {
@@ -53,6 +43,24 @@ class MyApp extends StatelessWidget {
         routerDelegate: _appRouter.delegate(),
         routeInformationParser: _appRouter.defaultRouteParser(),
       ),
+    );
+  }
+}
+
+class EasyLocalisationWrapper extends StatelessWidget {
+  const EasyLocalisationWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EasyLocalization(
+      path: 'assets/translations',
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ru'),
+      ],
+      assetLoader: const CodegenLoader(),
+      fallbackLocale: const Locale('en'),
+      child: MyApp(),
     );
   }
 }
